d8c450ca848d40f3aee5dce20297cc7241070ccc
alexruedadev <me@alexrueda.dev>
2023-01-13 00:42:17 +0100

    Added some custom theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
                 'glitch-green': '#00ff00',
                 'glitch-rose': '#f0f',
                 'glitch-blue': '#0ff',
+                'neon-blue-xs': '#08cff6',
+                'neon-blue-s': '#194262',
+                'neon-blue-m': '#0e2535',
+                'neon-blue-l': '#05131e',
+                'neon-blue-xl': '#020204'
+
             }
         },
         fontFamily: {

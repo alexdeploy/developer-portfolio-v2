fa8117302e2e2e5ef27f4cd265e99b9d06ed9f6c
alexruedadev <me@alexrueda.dev>
2023-01-13 00:44:58 +0100

    change fixed footer position

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="w-full fixed bottom-0 p-5 dark:border-gray-600 bg-black z-20">
+    <footer class="w-full  p-5 dark:border-gray-600 bg-black z-20"><!-- fixed bottom-0 -->
         <div class="flex max-w-7xl mx-auto justify-between items-center">
             
             <p class="dark:text-gray-400 text-gray-400 mx-5">Developed with ❤ by me.</p>

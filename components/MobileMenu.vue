971e5c869c42ce4016ce77fb66e5990e210d67f9
alexruedadev <me@alexrueda.dev>
2023-01-13 00:45:34 +0100

    Added styles and links to mobile menu

--- components/MobileMenu.vue
+++ components/MobileMenu.vue
@@ -1,16 +1,35 @@
 <template>
-  <div class="w-full h-52 bg-slate-500">
-    <NuxtLink to="/">Home</NuxtLink>
-    <NuxtLink to="/about">About</NuxtLink>
+  <div class="w-full h-64 bg-slate-500 px-5 dark:text-gray-100 font-sf text-lg tracking-wider">
+    
+    <div id="links" class="flex flex-col">
+      <NuxtLink id="menu-link" class="py-3" to="/about">About</NuxtLink>
+      <NuxtLink id="menu-link" class="py-3" to="/projects">Projects</NuxtLink>
+      <NuxtLink id="menu-link" class="py-3" to="/blog">My notes</NuxtLink>
+    </div>
+
+    <Switch class="flex py-3 justify-end" />
+
   </div>
 </template>
 
 <script>
 export default {
-
+  mounted(){
+    
+  }
 }
 </script>
 
 <style>
+.mobile-menu {
+  background: rgba(5,19,30,0.7);
+  backdrop-filter: blur(10px);
+  transition: top 0.4s;
+}
+
+#menu-link {
+  border-bottom: 1px solid;
+  @apply border-white/10;
+}
 
 </style>

b23323b75957426cb17e47488d136ab24ab5760b
alexruedadev <me@alexrueda.dev>
2023-01-13 00:49:41 +0100

    Changed style from switch theme component

--- components/Switch.vue
+++ components/Switch.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="flex items-center">
       <div
-        class="dark:bg-gray-700 bg-gray-200 switcher relative inline-flex h-[24px] w-[56px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+        class="dark:bg-gray-700 bg-gray-200 switcher relative inline-flex h-[24px] w-[56px] shrink-0 cursor-pointer rounded-md border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
       >
         <span class="sr-only">Use setting</span>
         <span
           aria-hidden="true"
           :class="enabled ? 'translate-x-8' : 'translate-x-0'"
-          class="pointer-events-none inline-block h-[20px] w-[21px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out"
+          class="pointer-events-none inline-block h-[20px] w-[21px] transform rounded-md bg-white shadow-lg ring-0 transition duration-200 ease-in-out"
         />
     </div>
     </div>

c65de128b1f991b2ff273bdce88979d10e14b96e
alexruedadev <me@alexrueda.dev>
2023-01-29 12:02:10 +0100

    about page templated

---
+++ pages/about-me.vue
@@ -0,0 +1,26 @@
+<template>
+      <section id="about" class="grid grid-cols-2 h-full w-full">
+
+        <div id="left" class="h-full w-full flex flex-col">
+          
+          <!-- windows bar -->
+          <div class="h-10 w-full">
+
+          </div>
+
+          <div class="h-full w-full">
+
+          </div>
+        
+        </div>
+
+        <div id="right" class="h-full w-full">
+
+        </div>
+
+      </section>
+</template>
+
+<style>
+
+</style>

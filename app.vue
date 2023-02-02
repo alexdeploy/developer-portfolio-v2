e50688891e1be6db92e8b1e13eea1fd40d80217d
alexruedadev <me@alexrueda.dev>
2023-02-02 18:29:05 +0100

    Refactored app.vue file.

--- app.vue
+++ app.vue
@@ -1,51 +1,6 @@
 <template>
-  <div class="box flex flex-col justify-between">
-  
-    <div id="body-content" class="flex flex-col lg:grid lg:grid-cols-6">
-
-
-
-      <MobileMenu class="lg:hidden" />
-
-      <div class="w-full h-full md:col-span-6 flex flex-col justify-between">
-        <!-- page navigation --> <AppHeader/>
-        <!-- page --> <NuxtPage/>
-        <!-- footer -->
-      </div>
-      
-    </div>
-
-    <AppFooter/>
-
-  </div>
+  <MobileMenu/>
+  <AppHeader/>
+  <NuxtPage/>
+  <AppFooter/>
 </template>
-
-<style>
-
-#logo-place {
-  border-bottom: 1px solid #1E2D3D;
-  border-right: 1px solid #1E2D3D;
-}
-
-#main-menu {
- /*  border-right: 1px solid #1E2D3D; */
-/*  display: flex;
- flex-direction: column; */
-}
-
-#body-content {
-  height: stretch;
-  width: stretch;
-}
-
-#social {
-  border-right: 1px solid #1E2D3D;
-  @apply justify-end
-}
-</style>
-
-<script>
-export default {
-
-}
-</script>

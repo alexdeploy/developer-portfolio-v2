6eb37931c2b3b8bbac30bf0fe3909b172ff2dd74
alexruedadev <me@alexrueda.dev>
2023-02-04 17:46:22 +0100

    Footer refactoring

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,42 +1,62 @@
 <template>
-    <footer class="flex w-full h-12 justify-between">
+    <footer class="flex justify-between border-top text-menu-text font-fira_retina text-sm">
 
-        <!-- social -->
-        <div class="flex text-sm h-full items-center border-right justify-end">
-            <p class="text-menu-text font-fira_retina px-6 h-full flex items-center justify-start">find me in:</p>
-            <NuxtLink to="https://twitter.com/alexdeploy" target="_blank" class="h-full flex justify-center">
-                <Icon name="ph:twitter-logo-fill" class="w-5 h-5 mx-4 my-auto text-hello-gray/60" />
+        <!-- social icons -->
+        <div id="social-icons" class="flex">
+            <span class="h-full flex justify-center items-center border-right px-5">
+                find me in:
+            </span>
+            <NuxtLink :to="social.twitter.url + social.twitter.user" target="_blank" class="flex justify-center items-center">
+                <Icon name="ri:twitter-fill" class="w-6 h-6 m-auto"/>
             </NuxtLink>
-            <NuxtLink to="https://linkedin.com/alexsrueda" target="_blank" class="h-full flex justify-center">
-                <Icon name="ph:linkedin-logo-fill" class="w-5 h-5 my-auto mx-4 text-hello-gray/60" />
-            </NuxtLink>
-            <NuxtLink to="https://instagram.com/alexdeploy" target="_blank" class="h-full flex justify-center">
-                <Icon name="ph:instagram-logo-fill" class="w-5 h-5 my-auto mx-4 text-hello-gray/60 " />
+            <NuxtLink :to="social.facebook.url + social.facebook.user" target="_blank" class="flex justify-center items-center">
+                <Icon name="ri:facebook-fill" class="w-6 h-6 m-auto"/>
             </NuxtLink>
         </div>
 
-        <!-- github -->
-        <div class="hidden md:flex justify-end text-sm h-full items-center">
-            <NuxtLink to="/" target="_blank" class="text-menu-text font-fira_retina pl-6 h-full flex items-center justify-end">
-                @alexdeploy
-                <Icon name="ri:github-fill" class="w-5 h-5 mx-3 text-hello-gray" />
-            </NuxtLink>
+        <!-- github user -->
+        <NuxtLink :to="social.github.url + social.github.user" target="_blank" class="hidden md:flex items-center px-5 border-left">
+            @{{ social.github.user }}
+            <Icon name="ri:github-fill" class="w-6 h-6 ml-2" />
+        </NuxtLink>
 
-        </div>
     </footer>
 </template>
 
 <style>
 
 footer {
-    border-top: 1px solid #1E2D3D;
+    height: 50px;
+    min-height: 50px;
 }
- footer a {
-    border-left: 1px solid #1E2D3D;
+
+footer a:hover {
+    background-color: #1e2d3d74;
+}
+
+#social-icons > a {
+    border-right: 1px solid #1E2D3D;
+    height: 100%;
+    width: 55px;
  }
 
- footer a:hover {
-    background-color: #1E2D3D;
- }
+#social-icons > a > svg {
+    opacity: 0.4;
+}
+
+#social-icons > a:hover svg {
+    opacity: 1;
+}
 
 </style>
+
+<script>
+export default {
+    name: 'AppFooter',
+    setup() {
+        return {
+            social: useRuntimeConfig().dev.contact.find_me_also_in.sources
+        }
+  },
+}
+</script>

727373f2da0e13b807074191b43f108f49c2c791
alexruedadev <me@alexrueda.dev>
2023-02-15 12:06:30 +0100

    Renamed component and line counter added

--- components/FormContentCode.vue
+++ components/FormContentCode.vue
@@ -1,5 +1,13 @@
 <template>
-    <div class="font-fira_retina text-white">
+    <div class="code-container flex font-fira_retina text-menu-text">
+    <div class="line-numbers lg:flex flex-col w-16 hidden">
+
+      <!-- line numbers and asteriscs -->
+      <div v-for="n in lines" class="grid grid-cols-2 justify-end" :key="n">
+        <span class="col-span-1 mr-3">{{ n }}</span>
+      </div>
+    </div>
+    <div class="font-fira_retina text-white text-container">
         <p>
             <span class="tag">
                 const
@@ -92,6 +100,7 @@
             
         </p>
     </div>
+    </div>
 </template>
 
 <script>
@@ -99,6 +108,7 @@ export default {
     data(){
         return {
             date: new Date().toDateString(),
+            lines: 0
         }
     },
     props: {
@@ -106,6 +116,27 @@ export default {
         email: String,
         message: String,
     },
+    mounted() {
+    this.updateLines();
+    window.addEventListener("resize", this.updateLines);
+    window.addEventListener("input", this.updateLines);
+    window.addEventListener("click", this.updateLines);
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.updateLines);
+    window.removeEventListener("click", this.updateLines);
+    window.addEventListener("input", this.updateLines);
+  },
+  methods: {
+    updateLines() {
+      const textContainer = this.$el.querySelector(".text-container");
+      const style = window.getComputedStyle(textContainer);
+      const fontSize = parseInt(style.fontSize);
+      const lineHeight = parseInt(style.lineHeight);
+      const maxHeight = textContainer.offsetHeight;
+      this.lines = Math.ceil(maxHeight / lineHeight);
+    }
+  }
 }
 </script>
 
@@ -120,5 +151,20 @@ export default {
 
 .arrow {
     color: #F8F8F8;
+}
+
+.code-container {
+  display: flex;
+  align-items: flex-start;
+}
+
+.line-numbers {
+  text-align: right;
+}
+
+.text-container {
+  width: 100%;
+  padding-left: 0px;
+  word-wrap: break-word;
 }
 </style>

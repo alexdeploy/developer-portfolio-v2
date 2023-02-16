7b2bc241af396f0021b0de1a6ab54afecce7e05c
alexruedadev <me@alexrueda.dev>
2023-02-16 23:03:42 +0100

    Added change route event on app.vue

--- app.vue
+++ app.vue
@@ -4,3 +4,19 @@
   <NuxtPage/>
   <AppFooter/>
 </template>
+
+<script>
+export default {
+  /**
+   * * Watch for route changes
+   * This event is triggered when the route changes.
+   * @param {Object} to - Route object
+   * @param {Object} from - Route object
+   */
+  watch: {
+    $route(to, from) {
+      console.log('De', from.fullPath, 'a', to.fullPath);
+    }
+  }
+}
+</script>

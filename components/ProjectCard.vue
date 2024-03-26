a4a074fc0ece5661bd556b8aa97551675707e837
alexdeploy <me@alexrueda.dev>
2024-03-26 22:27:08 +0100

    script setup and project-card component

---
+++ components/ProjectCard.vue
@@ -0,0 +1,80 @@
+<template>
+    <div id="project" :key="key" class="lg:mx-5">
+
+        <span class="flex text-sm my-3">
+            <h3 v-if="index == null" class="text-purplefy font-fira_bold mr-3">Project {{ key + 1 }}</h3>
+            <h3 v-else class="text-purplefy font-fira_bold mr-3">Project {{ index + 1 }}</h3>
+            <h4 class="font-fira_retina text-menu-text"> // {{ project.title }}</h4>
+        </span>
+
+        <div id="project-card" class="flex flex-col">
+            <div id="window">
+                <div class="absolute flex right-3 top-3">
+                <img v-for="tech in project.tech" :key="tech" :src="'/icons/techs/filled/' + tech + '.svg'" alt="" class="w-6 h-6 mx-1 hover:opacity-75">
+                </div>
+                <img id="showcase" :src="project.img" alt="" class="">
+            </div>
+
+            <div class="pb-8 pt-6 px-6 border-top">
+                <p class="text-menu-text font-fira_retina text-sm mb-5">
+                {{ project.description }}
+                </p>
+                <a id="view-button" :href="project.url" target="_blank" class="text-white font-fira_retina py-2 px-4 w-fit text-xs rounded-lg">
+                    view-project
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const { project, key, index } = defineProps(['project', 'key', 'index'])
+</script>
+
+<style scoped>
+#project {
+  min-width: 400px;
+  margin-bottom: 5px;
+}
+
+#project-card {
+  border: 1px solid #1E2D3D;
+  background-color: #011221;
+  border-radius: 15px;
+  max-width: 400px;
+}
+
+#window {
+  max-height: 120px;
+  position: relative;
+  overflow: hidden;
+}
+
+#showcase {
+  border-top-right-radius: 15px;
+  border-top-left-radius: 15px;
+}
+
+@media (max-width: 768px) {
+  #project {
+    min-width: 100%;
+  }
+}
+
+@media (min-width: 768px) {
+  #project {
+    width: 100%;
+    min-width: 100%;
+    padding-inline: 5px;
+  }
+}
+
+@media (min-width: 1350px) {
+  #project {
+    width: 100%;
+    min-width: 100%;
+    padding-inline: 20px;
+  }
+}
+
+</style>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,20 +1,20 @@
 <template>
-  <main v-if="!loading" class="flex flex-col flex-auto lg:flex-row overflow-hidden">
+  <main class="flex flex-col flex-auto lg:flex-row overflow-hidden">
 
     <div id="mobile-page-title">
       <h2>_projects</h2>
     </div>
 
     <!-- section title (mobile) -->
-    <div id="section-content-title" class="flex lg:hidden" @click="hiddeSection()">
-      <img class="section-arrow" src="/icons/arrow.svg">
-      <p class="font-fira_regular text-white text-sm">projects</p>
+    <div id="section-content-title" class="flex lg:hidden" @click="showFilters = !showFilters">
+      <img :class="showFilters ? 'section-arrow rotate-90' : 'section-arrow'" src="/icons/arrow.svg">
+      <span class="font-fira_regular text-white text-sm">projects</span>
     </div>
     
-    <div id="filter-menu" class="w-full flex-col border-right font-fira_regular text-menu-text hidden lg:flex">
+    <div v-if="showFilters" id="filter-menu" class="w-full flex-col border-right font-fira_regular text-menu-text lg:flex">
       <!-- title -->
       <div id="section-content-title" class="hidden lg:flex items-center min-w-full">
-        <img id="section-arrow-menu" src="/icons/arrow.svg" alt="" class="section-arrow mx-3 open">
+        <img id="section-arrow-menu" src="/icons/arrow.svg" alt="" class="section-arrow mx-3">
         <p class="font-fira_regular text-white text-sm">projects</p>
       </div>
 
@@ -62,41 +62,61 @@
             for these technologies
           </span>
         </div>
+        
+        <project-card
+          v-for="(project, key, index) in projects" 
+          :key="key" 
+          :index="index"
+          :project="project"
+        />
 
-        <div id="project" v-for="(project, key, index) in projects" :key="key" class="lg:mx-5">
-          <!-- title -->
-          <span class="flex text-sm my-3">
-            <h3 v-if="index == null" class="text-purplefy font-fira_bold mr-3">Project {{ key + 1 }}</h3>
-            <h3 v-else class="text-purplefy font-fira_bold mr-3">Project {{ index + 1 }}</h3>
-            <h4 class="font-fira_retina text-menu-text"> // {{ project.title }}</h4>
-          </span>
-
-          <!-- info -->
-          <div id="project-card" class="flex flex-col">
-            <div id="window">
-              <div class="absolute flex right-3 top-3">
-                <img v-for="tech in project.tech" :key="tech" :src="'/icons/techs/filled/' + tech + '.svg'" alt="" class="w-6 h-6 mx-1 hover:opacity-75">
-              </div>
-              <img id="showcase" :src="project.img" alt="" class="">
-            </div>
-
-            <div class="pb-8 pt-6 px-6 border-top">
-              <p class="text-menu-text font-fira_retina text-sm mb-5">
-                {{ project.description }}
-              </p>
-              <a id="view-button" :href="project.url" target="_blank" class="text-white font-fira_retina py-2 px-4 w-fit text-xs rounded-lg">
-                  view-project
-              </a>
-            </div>
-          </div>
-        </div>
       </div>
   </div>
 </main>
 </template>
 
-<style>
+<script setup>
+import { ref } from 'vue'
 
+const config = useRuntimeConfig()
+
+const techs = ['React', 'HTML', 'CSS', 'Vue', 'Angular', 'Gatsby', 'Flutter']
+const filters = ref(['all'])
+const showFilters = ref(true)
+const projects = ref(config.public.dev.projects)
+
+function filterProjects(tech) {
+  document.getElementById('icon-tech-' + tech).classList.toggle('active')
+  document.getElementById('title-tech-' + tech).classList.toggle('active')
+
+  const check = document.getElementById(tech)
+  if (check.checked) {
+    filters.value = filters.value.filter((item) => item !== 'all')
+    filters.value.push(tech)
+  } else {
+    filters.value = filters.value.filter((item) => item !== tech)
+    filters.value.length === 0 ? filters.value.push('all') : null
+  }
+  filters.value[0] == 'all' ? projects.value = config.public.dev.projects : projects.value = filterProjectsBy(filters.value)
+
+  if (projects.value.length === 0) {
+    document.getElementById('projects-case').classList.remove('grid')
+    document.getElementById('not-found').classList.remove('hidden')
+  } else {
+    document.getElementById('projects-case').classList.add('grid')
+    document.getElementById('not-found').classList.add('hidden')
+  }
+}
+
+function filterProjectsBy(filters) {
+  const projectArray = Object.values(config.public.dev.projects)
+  return projectArray.filter(project => {
+    return filters.some(filter => project.tech.includes(filter))
+  })
+}
+</script>
+
+<style>
 #filters {
   padding: 10px 25px;
 }
@@ -118,40 +138,12 @@
   color: white;
 }
 
-#projects-case {
-  /* padding: 100px; */
-}
-
-#project {
-  min-width: 400px;
-  margin-bottom: 5px;
-}
-
-#project-card {
-  border: 1px solid #1E2D3D;
-  background-color: #011221;
-  border-radius: 15px;
-  max-width: 400px;
-  /* max-height: 315px; */
-}
-
-#showcase {
-  border-top-right-radius: 15px;
-  border-top-left-radius: 15px;
-}
-
 #view-button {
   background-color: #1C2B3A;
 }
 
 #view-button:hover {
   background-color: #263B50;
-}
-
-#window {
-  max-height: 120px;
-  position: relative;
-  overflow: hidden;
 }
 
 input[type="checkbox"] {
@@ -204,20 +196,12 @@ input[type="checkbox"]:focus {
     padding: 0px 25px 40px;
   }
 
-  #project {
-    min-width: 100%;
-  }
 }
 
 @media (min-width: 768px) {
   #projects-case {
     grid-template-columns: repeat(2, minmax(0, 1fr));
     padding: 50px 50px 40px;
-  }
-  #project {
-    width: 100%;
-    min-width: 100%;
-    padding-inline: 5px;
   }
 }
 
@@ -226,11 +210,6 @@ input[type="checkbox"]:focus {
     grid-template-columns: repeat(3, minmax(0, 1fr));
     padding: 50px 80px 40px;
     /* padding: 100px 100px 40px; */
-  }
-  #project {
-    width: 100%;
-    min-width: 100%;
-    padding-inline: 20px;
   }
 }
 
@@ -243,72 +222,3 @@ input[type="checkbox"]:focus {
   }
 }
 </style>
-
-<script>
-export default {
-  setup() {
-    const config = useRuntimeConfig()
-    return {
-      config
-    }
-  },
-  data() {
-    return {
-      techs: ['React', 'HTML', 'CSS', 'Vue', 'Angular', 'Gatsby', 'Flutter'],
-      filters: ['all'],
-      projects: '',
-      loading: true,
-    };
-  },
-  mounted() {
-    this.projects = this.config.public.dev.projects;
-    this.loading = false;
-  },
-  methods: {
-    filterProjects(tech) {
-
-      document.getElementById('icon-tech-' + tech).classList.toggle('active'); // change tech icon opacity
-      document.getElementById('title-tech-' + tech).classList.toggle('active'); // change tech text color
-
-      const check = document.getElementById(tech);
-      if (check.checked) {
-        this.filters = this.filters.filter((item) => item !== 'all'); // remove 'all' from filters
-        this.filters.push(tech) // add tech to filters
-      } else {
-        this.filters = this.filters.filter((item) => item !== tech); // remove tech from filters
-        this.filters.length === 0 ? this.filters.push('all') : null; // add 'all' to filters if filters is empty
-      }
-      this.filters[0] == 'all' ? this.projects = this.config.public.dev.projects : this.projects = this.filterProjectsBy(this.filters);
-
-      if(this.projects.length === 0){
-        // set flex to projects-case
-        document.getElementById('projects-case').classList.remove('grid');
-        document.getElementById('not-found').classList.remove('hidden');
-        
-      }else{
-        // set grid to projects-case
-        document.getElementById('projects-case').classList.add('grid');
-        document.getElementById('not-found').classList.add('hidden');
-      }
-      
-    },
-    hiddeSection() {
-      document.getElementById('filter-menu').classList.toggle('hidden');
-      document.getElementsByClassName('section-arrow')[0].classList.toggle('rotate-90');
-    },
-    /**
-     * * Filter projects by techs
-     * * Each filter has to be an string with tech name that matches with project.tech !!
-     * ? If you want to filter projects that matches with ALL tech in filters, use 'every' instead of 'some'
-     * @param {*} filters is an array with techs names.
-     */
-    filterProjectsBy(filters) {
-      const projectArray = Object.values(this.config.public.dev.projects);
-      return projectArray.filter(project => {
-        return filters.some(filter => project.tech.includes(filter)); // change here your condition 'some' or 'every'
-      });
-    },
-  },
-};
-
-</script>

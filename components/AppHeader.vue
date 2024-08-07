0a7c4ce881cda92545df59dfe2f220855c7f73c0
Donia Skima <donia.skima@incedo.com>
2024-08-07 16:41:14 +0100

    fixing a bug

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <header id="navbar" class="w-full hidden lg:flex flex-col">
       <nav class="w-full flex justify-between border-bot">
-        <github-corner url="https://github.com/alexdeploy/developer-portfolio-v2" />
+        <github-corner url="https://github.com/doniaskima" />
           <div class="flex">
             <NuxtLink id="nav-logo" to="/">
               {{ config.dev.logo_name }}

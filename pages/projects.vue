1e7643208b2b415c3630686cd9a2bbd3afb59a05
Donia Skima <donia.skima@incedo.com>
2024-08-07 16:04:36 +0100

    feat:phone number

--- pages/projects.vue
+++ pages/projects.vue
@@ -80,7 +80,7 @@ import { ref } from 'vue'
 
 const config = useRuntimeConfig()
 
-const techs = ['React', 'HTML', 'CSS', 'Vue', 'Angular', 'Gatsby', 'Flutter']
+const techs = ['React', 'HTML', 'CSS', 'Vue', 'Angular', 'Gatsby', 'Flutter','Nestjs','Spring-boot','Typescript','RabbitMq']
 const filters = ref(['all'])
 const showFilters = ref(true)
 const projects = ref(config.public.dev.projects)
